94ddec35b17b2f64a34cda6783daf68ccd251a3c
Akash Sharma <soulakash20002@gmail.com>
2025-02-21 17:56:31 +0530

    update

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,12 @@
-import { Container, Flex } from "@chakra-ui/react";
+import { Container, Flex, Text, HStack, Button } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
 import React from "react";
+import { PiPlusSquare } from "react-icons/pi";
 
 const Navbar = () => {
+
+  // const { colorMode, toggleColorMode } = useColorMode();
+
   return (
     <Container maxW={"1140px"} px={4}>
       <Flex
@@ -9,7 +14,28 @@ const Navbar = () => {
         alignItems={"center"}
         justifyContent={"space-between"}
         flexDir={{base: "column", sm: "row"}}
-      ></Flex>
+      >
+        <Text
+					fontSize={{ base: "22", sm: "28" }}
+					fontWeight={"bold"}
+					textTransform={"uppercase"}
+					textAlign={"center"}
+					bgGradient={"linear(to-r, cyan.400, blue.500)"}
+					bgClip={"text"}
+				>
+					<Link to={"/"}>Product Store 🛒</Link>
+				</Text>
+
+        <HStack spacing={2} alignItems={"center"}>
+          <Link to={"/create"}>
+            <Button>
+              <PiPlusSquare fontSize={20} />
+            </Button>
+          </Link>
+          {/* <Button onClick={toggleColorMode}>
+            {colorMode === "light" ? "🌙" : "☀️"}</Button> */}
+        </HStack>
+      </Flex>
     </Container>
   );
 };
